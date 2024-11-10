79af23584b405295e0ea608e092cc4a6dd3f477f
Collins <149684399+coll254-strath@users.noreply.github.com>
2024-11-10 21:00:55 +0000

    Updated layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title:
-    `Ant Team : Increase productivity with AI-Powered Precision`,
+    `Ant Team : Increase team productivity with AI-Powered Precision`,
   description:
     'Where Collaborative Writing Meets Artificial Intelligence. All directly in Slack the moment an incident happens.',
   openGraph: {
